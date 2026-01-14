16719f6bb0e980e92d76cc1efa244072c58e1f49
ARSENE <youkapigor01@gmail.com>
2026-01-14 08:53:47 +0100

    Mise à jour de la gestion des coffres ( scripts, zone de detection, animation ouverture-fermeture )

--- Assets/scripts/CoffreSurprise.cs
+++ Assets/scripts/CoffreSurprise.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
-using TMPro; // Pour afficher le résultat au dessus du coffre
+using TMPro;
 
 public class CoffreSurprise : MonoBehaviour
 {
     private bool estOuvert = false;
-    public TMP_Text texteResultat; // Optionnel : pour afficher "+20s" au dessus du coffre
+    public TMP_Text texteResultat; // Compatible UI et 3D
+    private Animator anim; // La référence au système d'animation
 
-    // Cette fonction se déclenche quand la main ou le joueur touche le coffre
+    void Start()
+    {
+        // On récupère automatiquement l'Animator sur le même objet
+        anim = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        // On vérifie que c'est le joueur et que le coffre n'est pas déjà ouvert
         if (other.CompareTag("Player") && !estOuvert)
         {
             OuvrirCoffre();
@@ -20,34 +25,33 @@ public class CoffreSurprise : MonoBehaviour
     {
         estOuvert = true;
 
-        // 1. Trouver le GameManager pour agir sur le jeu
+        // 1. Lancer l'animation (Si l'animateur existe)
+        if (anim != null)
+        {
+            anim.SetTrigger("Ouvrir");
+        }
+
+        // 2. Gestion du Timer et Bonus
         GameTimer manager = FindObjectOfType<GameTimer>();
         if (manager == null) return;
 
-        // 2. Tirage au sort (0, 1 ou 2)
-        int tirage = Random.Range(0, 3); // Le 3 est exclu, donc ça donne 0, 1 ou 2
+        int tirage = Random.Range(0, 3);
 
         if (tirage == 0)
         {
-            // Bonus : +20 secondes
             manager.ModifierTemps(20f);
             AfficherTexte("BONUS\n+20s", Color.green);
         }
         else if (tirage == 1)
         {
-            // Malus : -20 secondes
             manager.ModifierTemps(-20f);
             AfficherTexte("PIEGE !\n-20s", Color.red);
         }
         else
         {
-            // Cadeau : La clé
             manager.RamasserCle();
             AfficherTexte("CLE\nTROUVEE !", Color.yellow);
         }
-
-        // Ici, tu pourras ajouter une animation d'ouverture plus tard
-        // GetComponent<Animator>().SetTrigger("Ouvrir");
     }
 
     void AfficherTexte(string message, Color couleur)
