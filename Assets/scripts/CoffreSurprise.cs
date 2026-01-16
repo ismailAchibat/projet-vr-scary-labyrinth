aa1c71d35b7a1f01b09a79d471e23c506fe02c70
ARSENE <youkapigor01@gmail.com>
2026-01-16 13:57:04 +0100

    Modification affichage des coffres, modification scripts gestion des coffres et premiere partie de l'interface.

--- Assets/scripts/CoffreSurprise.cs
+++ Assets/scripts/CoffreSurprise.cs
@@ -4,12 +4,11 @@ using TMPro;
 public class CoffreSurprise : MonoBehaviour
 {
     private bool estOuvert = false;
-    public TMP_Text texteResultat; // Compatible UI et 3D
-    private Animator anim; // La référence au système d'animation
+    public TMP_Text texteResultat;
+    private Animator anim;
 
     void Start()
     {
-        // On récupère automatiquement l'Animator sur le même objet
         anim = GetComponent<Animator>();
     }
 
@@ -25,31 +24,38 @@ public class CoffreSurprise : MonoBehaviour
     {
         estOuvert = true;
 
-        // 1. Lancer l'animation (Si l'animateur existe)
+        // 1. Lancer l'animation
         if (anim != null)
         {
             anim.SetTrigger("Ouvrir");
         }
 
-        // 2. Gestion du Timer et Bonus
+        // 2. Trouver le Manager
         GameTimer manager = FindObjectOfType<GameTimer>();
         if (manager == null) return;
 
+        // 3. Tirage au sort
         int tirage = Random.Range(0, 3);
 
         if (tirage == 0)
         {
+            // Bonus temps
             manager.ModifierTemps(20f);
             AfficherTexte("BONUS\n+20s", Color.green);
         }
         else if (tirage == 1)
         {
+            // Malus temps
             manager.ModifierTemps(-20f);
             AfficherTexte("PIEGE !\n-20s", Color.red);
         }
         else
         {
-            manager.RamasserCle();
+            // --- C'EST ICI QUE CA CHANGE ---
+
+            // On ajoute +1 au compteur de clés du manager
+            manager.clesTrouvees += 1;
+
             AfficherTexte("CLE\nTROUVEE !", Color.yellow);
         }
     }
